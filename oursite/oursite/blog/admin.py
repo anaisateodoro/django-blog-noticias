1bb2c2792755c52eb9e6374314cbab5aae70c9b3
anaisateodoro <anaisateodoro@hotmail.com>
2024-03-29 15:40:26 -0300

    :sparkles: feat: Página de login

---
+++ oursite/oursite/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+# Registrar nossas models aqui.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
