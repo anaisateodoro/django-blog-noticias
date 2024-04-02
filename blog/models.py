4e2ac78d6b60fd9ff73e7f2394bbe74fc8d6dc86
CRossafa <caroline.rossafa@gmail.com>
2024-04-02 15:57:43 -0300

    :sparkles: feat: Adiciona funcionalidade de feed ao projeto

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Crear nossas models aqui.
+from django.urls import reverse
+# Criar nossas models aqui.
 
 STATUS = (
     (0,"Draft"),
@@ -21,3 +22,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"slug": str(self.slug)})
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from .feeds import LatestPostFeed
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('feed/rss',LatestPostFeed(), name='post_feed'),
 ]
