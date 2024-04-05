79730c10a8ea2c6eb82cfa6c4896b08ad3d25919
Adriana Lúcia <108764670+Dricalucia@users.noreply.github.com>
2024-04-05 13:02:48 -0300

    configuração dos arquivos estaticos

--- oursite/settings.py
+++ oursite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',  
+    'django.contrib.staticfiles',  #gerenciar os arquivos estáticos
     'blog',
     'django.contrib.sitemaps'    
 ]
@@ -116,7 +116,11 @@ USE_TZ = False  # Desativa o suporte a fusos horários (time zones) - Brasília 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Configuração de arquivos estáticos
+STATIC_URL = '/static/'  #Location of static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]  #Location of static files
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
