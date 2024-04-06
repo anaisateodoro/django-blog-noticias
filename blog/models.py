2911ae944a22c6cb87881c38fdc6e15bfe639228
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-04-06 20:23:04 -0300

    :lipstick: feat: estilizacao de comentarios

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,6 @@ STATUS = (
     (1,"Publish")
 )
 
-
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -23,15 +22,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"slug": str(self.slug)})
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
+    name = models.CharField(max_length=200, verbose_name='nome')
+    email = models.EmailField(verbose_name='email')
+    body = models.TextField(verbose_name='comentário')
     created_on = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
     class Meta:
