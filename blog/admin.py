e20f3ea1720300b3d123ddc5b2acdaca9fa89ff0
anaisateodoro <anaisateodoro@hotmail.com>
2024-04-06 02:14:10 -0300

    :lipstick: feat: Estilização do editor post

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 # Registrar nossas models aqui.
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status','created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
-admin.site.register(Post, PostAdmin)
+
 @admin.register(Comment)
 class CommentAdm(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'active')
@@ -19,3 +21,5 @@ class CommentAdm(admin.ModelAdmin):
     def approve_comments(self, request,queryset):
         queryset.update(active=True)
     approve_comments.short_description = 'Approve comments'
+
+admin.site.register(Post, PostAdmin)
