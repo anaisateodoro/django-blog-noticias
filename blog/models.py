2e91ddb22eeec147b619f8ce598d6b8b77049c99
Luciane Fernandes Roque <lucianefernandescdf@gmail.com>
2024-04-10 20:16:46 -0300

    Revert ":sparkles: feat: inserido paginação"

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime
+from django.utils.html import format_html
+
+
 # Criar nossas models aqui.
 
 STATUS = (
@@ -9,6 +13,7 @@ STATUS = (
 )
 
 class Post(models.Model):
+    image = models.ImageField(upload_to='images', verbose_name='imagem',  default='images/default.jpg')
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
@@ -16,7 +21,7 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-
+    
     class Meta:
         ordering = ['-created_on']
 
@@ -34,8 +39,25 @@ class Comment(models.Model):
     body = models.TextField(verbose_name='comentário')
     created_on = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
+
     class Meta:
         ordering = ['created_on']
 
     def __str__(self):
         return 'Comment {} by {}'.format(self.body, self.name)
+    
+class Contato(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    data = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(verbose_name='email')
+    telefone = models.CharField(null=True, blank=True)
+    assunto = models.CharField(max_length=100)
+    mensagem = models.TextField(verbose_name='deixe sua mensagem')
+
+    class Meta:
+        verbose_name = 'Contato'
+        ordering = ['-data']
+        
+    def __str__(self):
+        return self.nome
